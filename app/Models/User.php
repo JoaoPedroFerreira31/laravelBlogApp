fb69609bd623a215ca2a051fd4a2a3340f7d89e7
JoaoPedroFerreira <jpmferreira@xnr-sisbi.com>
2022-12-23 14:19:02 +0000

    add laravel-permission package

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use App\Models\Post;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, UuidTrait;
+    use HasApiTokens, HasFactory, Notifiable, UuidTrait, HasRoles;
 
     /**
      * The attributes that are mass assignable.
