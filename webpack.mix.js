df1b9834ac1464c21a8efea50c27d25959f30e83
JoaoPedroFerreira31 <jpmferreira31@@gmail.com>
2023-04-17 10:52:47 +0000

    Installed laravel-echo and pusher.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -24,6 +24,7 @@ require('laravel-mix-serve');
 mix.serve('php artisan lang:js');
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/utils.js', 'public/js')
+    .js('resources/js/bootstrap.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('tailwindcss'),
         require('autoprefixer'),
